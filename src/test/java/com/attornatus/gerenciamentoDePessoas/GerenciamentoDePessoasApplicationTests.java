60728873c7c5b3928fb8739a23ae5f86d7c81a45
RicardoCunha42 <ricardotoms42@gmail.com>
2023-01-10 13:32:19 -0300

    addBaseProject

---
+++ src/test/java/com/attornatus/gerenciamentoDePessoas/GerenciamentoDePessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attornatus.gerenciamentoDePessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDePessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
